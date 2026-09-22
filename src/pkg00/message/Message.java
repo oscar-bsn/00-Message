c8e2522412ee087dd06ab11823495bc3b17b1cf9
obesnier <obesnier@350028R-F115X07.lise.kreizenn.bzh>
2026-09-22 15:32:34 +0200

    1er commit: ajout fichier

---
+++ src/pkg00/message/Message.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg00.message;
+
+/**
+ *
+ * @author obesnier
+ */
+public class Message {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Message random !");
+    }
+    
+}
