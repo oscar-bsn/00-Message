95a6665c3984a91054edfffaa468f8f85026cf2e
obesnier <obesnier@350028R-F115X07.lise.kreizenn.bzh>
2026-09-25 10:50:11 +0200

    test 2e cours pour commit

--- src/pkg00/message/Message.java
+++ src/pkg00/message/Message.java
@@ -28,7 +28,7 @@ public class Message {
         System.out.println("Avant permutation : x="+x+" y="+y);
         y = x ;
         x = temp ;
-        System.out.println("Apres permutation : x="+x+" y="+y); */
+        System.out.println("Apres permutation : x="+x+" y="+y); ze*/ 
         
     }
     
