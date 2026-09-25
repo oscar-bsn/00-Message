63b5113d4ca5751047a60dacd2edcc9977c16c27
obesnier <obesnier@350028R-F115X07.lise.kreizenn.bzh>
2026-09-25 10:39:21 +0200

    test 2e cours

--- src/pkg00/message/Message.java
+++ src/pkg00/message/Message.java
@@ -5,17 +5,31 @@
 package pkg00.message;
 
 /**
- *
  * @author obesnier
  */
+import java.util.Scanner;
 public class Message {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        /* Exo 2
         System.out.println("Message ligne 1");
-        System.out.println("Message ligne 2");
+        System.out.println("Message ligne 2"); */
+        
+        /* Exo 3
+        System.out.println("Message ligne 1");
+        System.out.println("Message ligne 2"); */
+        
+        /* Exo 4 
+        Scanner wp = new Scanner(System.in);
+        System.out.println("Saisir x :");
+        int x = wp.nextInt() ;
+        System.out.println("Saisir y :");
+        int y = wp.nextInt() ;
+        int temp = y ;
+        System.out.println("Avant permutation : x="+x+" y="+y);
+        y = x ;
+        x = temp ;
+        System.out.println("Apres permutation : x="+x+" y="+y); */
+        
     }
     
 }
